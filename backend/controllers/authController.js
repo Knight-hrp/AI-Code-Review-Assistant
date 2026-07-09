34a680debb14e33d0dcfa5683dbe112ce4ede06c
Knight-hrp <patidarrishabh20@gmail.com>
2026-07-09 16:44:23 +0530

    login backend

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcrypt");
 const pool = require("../config/db");
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
     try {
@@ -35,6 +36,61 @@ const register = async (req, res) => {
     }
 };
 
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const result = await pool.query(
+            "SELECT * FROM users WHERE email = $1",
+            [email]
+        );
+
+        if (result.rows.length === 0) {
+            return res.status(400).json({
+                message: "Invalid email or password",
+            });
+        }
+
+        const user = result.rows[0];
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({
+                message: "Invalid email or password",
+            });
+        }
+
+        const token = jwt.sign(
+            {
+                id: user.id,
+                email: user.email,
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "1d",
+            }
+        );
+
+        res.status(200).json({
+            message: "Login Successful",
+            token,
+            user: {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+            },
+        });
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            message: "Server Error",
+        });
+    }
+
+}
+
 module.exports = {
-    register,
+    register, login
 };
